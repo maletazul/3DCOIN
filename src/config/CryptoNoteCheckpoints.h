27d41fef67b9eb7c4b3f2208d0adf5acea6b6dc1
maletazul <luissilva@maletazul.pt>
2022-03-27 15:32:46 +0100

    Update CryptoNoteCheckpoints.h

--- src/config/CryptoNoteCheckpoints.h
+++ src/config/CryptoNoteCheckpoints.h
@@ -17,6 +17,5 @@ namespace CryptoNote
     };
 
     const std::initializer_list<CheckpointData> CHECKPOINTS = {
-        {0, "d7b2a50751f49eb0b916450e47df9f8728e890ea089e4f433dd35996bf801050"},
-        {2, "f216b8e48ba8d705a2308f4bc063e76da9db997ef9dc6813c6299780d1b0a5c0"}};
+};
 } // namespace CryptoNote
